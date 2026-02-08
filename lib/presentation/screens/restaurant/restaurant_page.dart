1fe8dd4e4711c45902aad1536ab75b492200272b
İsmail Durcan <ismaildurcan40@gmail.com>
2026-02-08 14:02:36 +0300

    Refactor RestaurantPage layout to use NestedScrollView and improve UI structure

--- lib/presentation/screens/restaurant/restaurant_page.dart
+++ lib/presentation/screens/restaurant/restaurant_page.dart
@@ -50,169 +50,240 @@ class _RestaurantPageState extends State<RestaurantPage> {
     return Scaffold(
       body: restaurant == null
           ? Center(child: CircularProgressIndicator())
-          : Column(
-              children: [
-                Container(
-                  height: 320,
-                  decoration: BoxDecoration(
-                    image: DecorationImage(
-                      image: AssetImage(restaurant!.coverImageUrl!),
-                      fit: BoxFit.cover,
+          : DefaultTabController(
+              length: 3,
+              child: NestedScrollView(
+                headerSliverBuilder: (context, innerBoxIsScrolled) {
+                  return [
+                    SliverToBoxAdapter(
+                      child: Container(
+                        height: 320,
+                        decoration: BoxDecoration(
+                          image: DecorationImage(
+                            image: AssetImage(restaurant!.coverImageUrl!),
+                            fit: BoxFit.cover,
+                          ),
+                          borderRadius: BorderRadius.vertical(
+                            bottom: Radius.circular(30),
+                          ),
+                        ),
+                        child: Padding(
+                          padding: const EdgeInsets.symmetric(horizontal: 24),
+                          child: SafeArea(
+                            child: Column(
+                              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                              children: [
+                                Row(
+                                  crossAxisAlignment: CrossAxisAlignment.start,
+                                  mainAxisAlignment:
+                                      MainAxisAlignment.spaceBetween,
+                                  children: [
+                                    Container(
+                                      decoration: BoxDecoration(
+                                        color: Theme.of(context)
+                                            .colorScheme
+                                            .onSecondary
+                                            .withAlpha(150),
+                                        borderRadius: BorderRadius.circular(15),
+                                      ),
+                                      child: IconButton(
+                                        iconSize: 38,
+                                        icon: Icon(Icons.chevron_left_sharp),
+                                        onPressed: () {
+                                          Navigator.pop(context);
+                                        },
+                                      ),
+                                    ),
+                                    Container(
+                                      decoration: BoxDecoration(
+                                        color: Theme.of(context)
+                                            .colorScheme
+                                            .onSecondary
+                                            .withAlpha(150),
+                                        borderRadius: BorderRadius.circular(15),
+                                      ),
+                                      child: IconButton(
+                                        iconSize: 28,
+                                        onPressed: () {},
+                                        icon: Icon(Icons.favorite_border),
+                                      ),
+                                    ),
+                                  ],
+                                ),
+                                Align(
+                                  alignment: Alignment.centerRight,
+                                  child: Column(
+                                    crossAxisAlignment: CrossAxisAlignment.end,
+                                    mainAxisAlignment: MainAxisAlignment.end,
+                                    children: [
+                                      restaurant?.category != null
+                                          ? Container(
+                                              padding: EdgeInsets.symmetric(
+                                                horizontal: 8,
+                                                vertical: 2,
+                                              ),
+                                              decoration: BoxDecoration(
+                                                color: Theme.of(context)
+                                                    .colorScheme
+                                                    .primary
+                                                    .withAlpha(200),
+                                                borderRadius:
+                                                    BorderRadius.circular(6),
+                                              ),
+                                              child: Text(
+                                                restaurant?.category?.name ??
+                                                    "N/A",
+                                                style: TextStyle(
+                                                  fontSize: 16,
+                                                  color: Colors.white,
+                                                ),
+                                              ),
+                                            )
+                                          : SizedBox.shrink(),
+                                      SizedBox(height: 4),
+                                      restaurant?.cuisine != null
+                                          ? Container(
+                                              padding: EdgeInsets.symmetric(
+                                                horizontal: 8,
+                                                vertical: 2,
+                                              ),
+                                              decoration: BoxDecoration(
+                                                color: Theme.of(context)
+                                                    .colorScheme
+                                                    .primary
+                                                    .withAlpha(200),
+                                                borderRadius:
+                                                    BorderRadius.circular(6),
+                                              ),
+                                              child: Text(
+                                                restaurant?.cuisine?.name ??
+                                                    "N/A",
+                                                style: TextStyle(
+                                                  fontSize: 16,
+                                                  color: Colors.white,
+                                                ),
+                                              ),
+                                            )
+                                          : SizedBox.shrink(),
+                                    ],
+                                  ),
+                                ),
+                              ],
+                            ),
+                          ),
+                        ),
+                      ),
                     ),
-                    borderRadius: BorderRadius.vertical(
-                      bottom: Radius.circular(30),
+                    SliverToBoxAdapter(
+                      child: Padding(
+                        padding: const EdgeInsets.symmetric(horizontal: 24),
+                        child: Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: [
+                            SizedBox(height: 26),
+                            RestaurantMetaInfo(
+                              rating: restaurant?.rating ?? 0,
+                              deliveryFee: restaurant?.deliveryFee ?? 0,
+                              deliveryTime: restaurant?.deliveryTimeRange ?? "",
+                            ),
+                            SizedBox(height: 16),
+                            Text(
+                              restaurant?.name ?? "N/A",
+                              style: TextStyle(
+                                fontSize: 24,
+                                fontWeight: FontWeight.bold,
+                              ),
+                            ),
+                            SizedBox(height: 8),
+                            Text(
+                              restaurant?.description ?? "N/A",
+                              style: TextStyle(
+                                fontSize: 14,
+                                color: Theme.of(
+                                  context,
+                                ).colorScheme.onSurface.withAlpha(128),
+                              ),
+                            ),
+                            SizedBox(height: 20),
+                          ],
+                        ),
+                      ),
                     ),
-                  ),
-                  child: Padding(
-                    padding: const EdgeInsets.symmetric(horizontal: 24),
-                    child: SafeArea(
-                      child: Row(
-                        crossAxisAlignment: CrossAxisAlignment.start,
-                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    SliverToBoxAdapter(
+                      child: Padding(
+                        padding: const EdgeInsets.symmetric(horizontal: 24),
+                        child: ButtonsTabBar(
+                          labelStyle: TextStyle(
+                            fontSize: 16,
+                            color: Theme.of(
+                              context,
+                            ).colorScheme.onPrimary.withAlpha(200),
+                          ),
+                          contentPadding: EdgeInsets.symmetric(
+                            horizontal: 16,
+                            vertical: 8,
+                          ),
+                          decoration: BoxDecoration(
+                            color: Theme.of(context).colorScheme.primary,
+                            borderRadius: BorderRadius.circular(50),
+                          ),
+                          unselectedDecoration: BoxDecoration(
+                            color: Theme.of(context).colorScheme.secondary,
+                            borderRadius: BorderRadius.circular(50),
+                            border: Border.all(
+                              color: Theme.of(context).colorScheme.onSurface,
+                              width: 1.5,
+                            ),
+                          ),
+                          tabs: [
+                            Tab(text: "Menu"),
+                            Tab(text: "Drinks"),
+                            Tab(text: "Desserts"),
+                          ],
+                        ),
+                      ),
+                    ),
+                    SliverToBoxAdapter(child: SizedBox(height: 16)),
+                  ];
+                },
+                body: Padding(
+                  padding: const EdgeInsets.symmetric(horizontal: 24),
+                  child: TabBarView(
+                    clipBehavior: Clip.none,
+                    children: [
+                      GridView.builder(
+                        clipBehavior: Clip.none,
+                        padding: EdgeInsets.only(bottom: 24),
+                        gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+                          crossAxisCount: 2,
+                          mainAxisSpacing: 16,
+                          crossAxisSpacing: 16,
+                          childAspectRatio: 155 / 150,
+                        ),
+                        itemBuilder: (context, index) {
+                          return menuItemCard();
+                        },
+                        itemCount: 20,
+                      ),
+                      ListView(
+                        padding: EdgeInsets.only(bottom: 16),
                         children: [
-                          Container(
-                            decoration: BoxDecoration(
-                              color: Theme.of(
-                                context,
-                              ).colorScheme.onSecondary.withAlpha(150),
-                              borderRadius: BorderRadius.circular(15),
-                            ),
-                            child: IconButton(
-                              iconSize: 38,
-                              icon: Icon(Icons.chevron_left_sharp),
-                              onPressed: () {
-                                Navigator.pop(context);
-                              },
-                            ),
-                          ),
-                          Container(
-                            decoration: BoxDecoration(
-                              color: Theme.of(
-                                context,
-                              ).colorScheme.onSecondary.withAlpha(150),
-                              borderRadius: BorderRadius.circular(15),
-                            ),
-                            child: IconButton(
-                              iconSize: 28,
-                              onPressed: () {},
-                              icon: Icon(Icons.favorite_border),
-                            ),
-                          ),
+                          Text('Drinks Content'),
+                          // Drinks buraya eklenecek
                         ],
                       ),
-                    ),
-                  ),
-                ),
-                Padding(
-                  padding: const EdgeInsets.symmetric(horizontal: 24),
-                  child: Column(
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    children: [
-                      SizedBox(height: 26),
-                      RestaurantMetaInfo(
-                        rating: restaurant?.rating ?? 0,
-                        deliveryFee: restaurant?.deliveryFee ?? 0,
-                        deliveryTime: restaurant?.deliveryTimeRange ?? "",
+                      ListView(
+                        padding: EdgeInsets.only(bottom: 16),
+                        children: [
+                          Text('Desserts Content'),
+                          // Desserts buraya eklenecek
+                        ],
                       ),
-                      SizedBox(height: 16),
-                      Text(
-                        restaurant?.name ?? "N/A",
-                        style: TextStyle(
-                          fontSize: 24,
-                          fontWeight: FontWeight.bold,
-                        ),
-                      ),
-                      SizedBox(height: 8),
-                      Text(
-                        restaurant?.description ?? "N/A",
-                        style: TextStyle(
-                          fontSize: 14,
-                          color: Theme.of(
-                            context,
-                          ).colorScheme.onSurface.withAlpha(128),
-                        ),
-                      ),
-                      SizedBox(height: 20),
                     ],
                   ),
                 ),
-                Expanded(
-                  child: Padding(
-                    padding: const EdgeInsets.symmetric(horizontal: 24),
-                    child: menuCategories(context),
-                  ),
-                ),
-              ],
-            ),
-    );
-  }
-
-  DefaultTabController menuCategories(BuildContext context) {
-    return DefaultTabController(
-      length: 3,
-      child: Column(
-        crossAxisAlignment: CrossAxisAlignment.start,
-        children: [
-          ButtonsTabBar(
-            labelStyle: TextStyle(
-              fontSize: 16,
-              color: Theme.of(context).colorScheme.onPrimary.withAlpha(200),
-            ),
-            contentPadding: EdgeInsets.symmetric(horizontal: 16, vertical: 8),
-            decoration: BoxDecoration(
-              color: Theme.of(context).colorScheme.primary,
-              borderRadius: BorderRadius.circular(50),
-            ),
-            unselectedDecoration: BoxDecoration(
-              color: Theme.of(context).colorScheme.secondary,
-              borderRadius: BorderRadius.circular(50),
-              border: Border.all(
-                color: Theme.of(context).colorScheme.onSurface,
-                width: 1.5,
               ),
             ),
-            tabs: [
-              Tab(text: "Menu"),
-              Tab(text: "Reviews"),
-              Tab(text: "Info"),
-            ],
-          ),
-          SizedBox(height: 16),
-          Expanded(
-            child: TabBarView(
-              children: [
-                GridView.builder(
-                  padding: EdgeInsets.only(bottom: 16),
-                  gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
-                    crossAxisCount: 2,
-                    mainAxisSpacing: 16,
-                    crossAxisSpacing: 16,
-                    childAspectRatio: 155 / 150,
-                  ),
-                  itemBuilder: (context, index) {
-                    return menuItemCard();
-                  },
-                  itemCount: 10,
-                ),
-                ListView(
-                  padding: EdgeInsets.only(bottom: 16),
-                  children: [
-                    Text('Reviews Content'),
-                    // Reviews buraya eklenecek
-                  ],
-                ),
-                ListView(
-                  padding: EdgeInsets.only(bottom: 16),
-                  children: [
-                    Text('Info Content'),
-                    // Restaurant info buraya eklenecek
-                  ],
-                ),
-              ],
-            ),
-          ),
-        ],
-      ),
     );
   }
 
@@ -244,6 +315,7 @@ class _RestaurantPageState extends State<RestaurantPage> {
                 child: Container(
                   width: 114,
                   height: 75,
+                  clipBehavior: Clip.none,
                   decoration: BoxDecoration(
                     borderRadius: BorderRadius.vertical(
                       top: Radius.circular(25),
